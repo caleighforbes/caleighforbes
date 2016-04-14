ba5b82121590474da2942d1cd3015f152d25fafa
caleighforbes <caleigh.forbes@gmail.com>
2016-04-14 19:34:15 -0400

    add content and formatting

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+
+// HOMEPAGE SLIDESHOW
+
+ $.backstretch([
+       "images/slideshow/IMG_7275.jpg"
+     , "images/slideshow/IMG_9751.jpg"
+     , "images/slideshow/IMG_9755.jpg"
+     , "images/slideshow/IMG_9791.jpg"
+   ], {duration: 3000, fade: 750});
+
+
+})
---
+++ js/project.js
@@ -0,0 +1,16 @@
+$(function(){
+
+// PROJECT PAGE SLIDESHOW
+
+  $('.project-slideshow').slick({
+    arrows: true,
+    adaptiveHeight: true,
+    draggable: true,
+    initialSlide: 0,
+    variableWidth: false,
+    arrows: true,
+    speed: 300,
+    dots: true
+  });
+
+})
