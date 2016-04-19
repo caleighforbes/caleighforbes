8d39e16da1ce2f13d0cc6f38a046f90759f3e113
caleighforbes <caleigh.forbes@gmail.com>
2016-04-18 21:46:26 -0400

    add edits

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(function(){
 // HOMEPAGE SLIDESHOW
 
  $.backstretch([
-       "images/slideshow/IMG_7275.jpg"
+       "images/slideshow/IMG_9732.jpg"
      , "images/slideshow/IMG_9751.jpg"
      , "images/slideshow/IMG_9755.jpg"
      , "images/slideshow/IMG_9791.jpg"
